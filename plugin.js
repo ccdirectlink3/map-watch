4caff2f1f2dc5fcb3ac913eeef10b7c1cd144ae6
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-06-23 17:50:16 +0300

    upgrade to ccloader 3

--- plugin.js
+++ plugin.js
@@ -1,116 +1,94 @@
-const fs = require('fs');
+if (typeof require === 'undefined') {
+	throw new Error('map-watch does not work in the browser')
+}
 
-export default class MapWatcher extends Plugin {
+const fs = require('fs');
+const fsP = fs.promises;
+
+const ASSETS_DIR = 'assets/';
+const MAPS_DIR = 'data/maps/';
+const MAP_FILE_EXTENSION = '.json';
+
+export default class MapWatcher {
 	constructor() {
-		super();
 		this.watchloads = 0;
 		this.lastPos = {x: 0, y: 0, z: 0};
 	}
 
-	prestart() {
-		if (window.isBrowser) {
-			throw new Error('MapWatcher does not work in the browser.');
+	poststart() {
+		fs.watch(ASSETS_DIR, { recursive: true }, this._fileChanged.bind(this));
+		ig.game.addons.levelLoaded.push(this);
+	}
+
+	/**
+	 *
+	 * @param {string} event
+	 * @param {string} assetPath
+	 */
+	async _fileChanged(event, assetPath) {
+		assetPath = assetPath.replace(/\\/g, '/');
+		if (!assetPath.endsWith(MAP_FILE_EXTENSION)) {
+			return;
 		}
 
-		fs.watch('./assets/', {
-			recursive: true
-		}, (event, filename) => this._fileChanged(event, filename));
-        
-		const self = this;
-		sc.CrossCode.inject({
-			init(...args) {
-				this.parent(...args);
-				this.addons.levelLoaded.push(self);
+		const fullFilePath = ASSETS_DIR + assetPath;
+		if (!(await this._isFile(fullFilePath))) {
+			return;
+		}
+
+		const mod = this._getModFromPath(fullFilePath);
+		if (mod != null) {
+			if (!fullFilePath.startsWith(mod.assetsDirectory)) {
+				return;
 			}
-		});
-	}
-
-	/**
-     * 
-     * @param {string} event 
-     * @param {string} filename 
-     */
-	async _fileChanged(event, filename) {
-		if (this._isMap(filename) && await this._exists(filename)) {
-			this._addToAssets(filename);
-			this._loadMap(filename);
+			assetPath = fullFilePath.slice(mod.assetsDirectory.length);
 		}
+
+		if (!assetPath.startsWith(MAPS_DIR)) {
+			return;
+		}
+
+		if (mod != null) {
+			ccmod3.resources.assetOverridesTable.set(assetPath, fullFilePath);
+		}
+
+		this._loadMap(assetPath);
 	}
 
 	/**
-     * 
-     * @param {string} filename 
-     */
-	_isMap(filename) {
-		return !!filename
-        && filename.endsWith('.json')
-        && /^(mods\/[^/]+\/assets\/)?data\/maps\//.test(filename.replace(/\\/g, '/'));
-	}
-
-	/**
-	 * 
-	 * @param {string} filename 
+	 *
+	 * @param {string} path
 	 * @returns {Promise<boolean>}
 	 */
-	_exists(filename) {
-		return new Promise((resolve) => {
-			fs.stat('./assets/' + filename, (err, stat) => {
-				if (err) {
-					return resolve(false);
-				}
-				resolve(stat.isFile());
-			});
-		});
-	}
-
-	/**
-     * 
-     * @param {string} filename 
-     */
-	_getMapName(filename) {
-		return filename
-			.replace(/\\/g, '/')
-			.replace(/^mods\/[^/]+\/assets\//, '')
-			.replace(/^data\/maps\//, '')
-			.replace(/\.json$/, '')
-			.replace(/\//g, '.');
-	}
-
-	/**
-	 * 
-	 * @param {string} filename 
-	 */
-	_getDirName(filename) {
-		filename = filename.replace(/\\/g, '/');
-		return 'assets/' + filename.substr(0, filename.indexOf('/', 5)) + '/';
-	}
-
-	/**
-	 * 
-	 * @param {string} filename 
-	 */
-	_getMod(filename) {
-		const baseDir = this._getDirName(filename);
-		return window.activeMods.find(mod => mod.baseDirectory === baseDir);
-	}
-
-	/**
-	 * 
-	 * @param {string} filename 
-	 */
-	_addToAssets(filename) {
-		const mod = this._getMod(filename);
-		if (mod) {
-			mod.assets.push(filename.replace(/\\/g, '/'));
+	async _isFile(path) {
+		try {
+			return (await fsP.stat(path)).isFile();
+		} catch (err) {
+			if (err.code === 'ENOENT') return false;
+			throw err;
 		}
 	}
 
 	/**
-     * 
-     * @param {string} filename 
-     */
-	_loadMap(filename) {
-		const name = this._getMapName(filename);
+	 *
+	 * @param {string} fullFilePath
+	 * @returns {{ assetsDirectory: string } | null}
+	 */
+	_getModFromPath(fullFilePath) {
+		for (const mod of modloader.loadedMods.values()) {
+			if (fullFilePath.startsWith(mod.baseDirectory)) {
+				return mod;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 *
+	 * @param {string} filePath
+	 */
+	_loadMap(filePath) {
+		const name = this._getMapName(filePath);
 		if (ig.game.playerEntity && name === ig.game.mapName) {
 			this.watchloads++;
 			this.lastPos = ig.game.playerEntity.coll.pos;
@@ -121,7 +99,21 @@ export default class MapWatcher extends Plugin {
 			ig.game.teleport(name);
 		}
 	}
-    
+
+	/**
+	 *
+	 * @param {string} assetPath
+	 */
+	_getMapName(assetPath) {
+		return assetPath
+			.slice(MAPS_DIR.length, assetPath.length - MAP_FILE_EXTENSION.length)
+			.replace(/\//g, '.');
+	}
+
+	/**
+	 *
+	 * @returns {void}
+	 */
 	onLevelLoaded() {
 		if (this.watchloads) {
 			this.watchloads--;
